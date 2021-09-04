4fba5353a75b950b4728b414dd75ce9796702fa2
PaulHuerta <paulhuerta888@gmail.com>
2021-09-03 21:21:46 -0500

    Update

--- create/js/index.js
+++ create/js/index.js
@@ -4,39 +4,65 @@ var buttonCopy = document.getElementById("copy_text");
 
 button.addEventListener("click", function () {
   var names = document.getElementById("name").value;
-  var sex_h = document.getElementById("sex_h").checked;
-  var sex_m = document.getElementById("sex_m").checked;
-  var sex_o = document.getElementById("sex_o").checked;
-  var texta = document.getElementById("text_a").value;
 
-  if (sex_h == true) {
-    var sex = "h";
-  } else if (sex_m == true) {
-    var sex = "m";
-  } else if (sex_o == true) {
-    var sex = "o";
+  if (names.length > 1) {
+    var sex_h = document.getElementById("sex_h").checked;
+    var sex_m = document.getElementById("sex_m").checked;
+    var sex_o = document.getElementById("sex_o").checked;
+    var texta = document.getElementById("text_a").value;
+
+    if (sex_h == true) {
+      var sex = "h";
+    } else if (sex_m == true) {
+      var sex = "m";
+    } else if (sex_o == true) {
+      var sex = "o";
+    } else {
+      var sex = "";
+    }
+    linkNOTshort =
+      "https://paulhuerta.dev/projects/YouWantToBeMy/?sex=" +
+      sex +
+      "&name=" +
+      names +
+      "&text=" +
+      texta;
+
+    function InputSetURL(url) {
+      inputLink.value = url;
+    }
+
+    fetch("https://api.tinyurl.com/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization:
+          "Bearer HKgZSYSWogCS2s9hu5szKgQoaWQSj1faNIpr4hUcYBoFyGUg6IWUSv8tHW6J",
+      },
+      body: JSON.stringify({
+        url: linkNOTshort,
+        domain: "tiny.one",
+      }),
+    })
+      .then((response) => response.json())
+      .then((code) => {
+        if (code.data !== undefined) {
+          var AllData = code.data;
+          InputSetURL(AllData.tiny_url);
+        } else {
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Hubo un error al acortar el URL",
+          });
+        }
+      });
   } else {
-    var sex = "";
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Parece que no has ingresado un nombre.",
+    });
   }
-
-  linkNOTshort =
-    "https://paulhuerta.dev/proyects/YouWantToBeMy/?sex=" +
-    sex +
-    "&name=" +
-    names +
-    "&text=" +
-    texta;
-
-  var result = fetch("https://api.tinyurl.com/create", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      Authorization:
-        "Bearer HKgZSYSWogCS2s9hu5szKgQoaWQSj1faNIpr4hUcYBoFyGUg6IWUSv8tHW6J",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ url: linkNOTshort, domain: "tiny.one" }),
-  });
-
-  console.log(result)
 });
